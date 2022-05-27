86f52638bba299935da8b932d4ae08644dee0213
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-05-27 23:37:27 +0200

    IPNS record signing web3 storage direct api

--- src/app/services/ipfs/web3-storage.ts
+++ src/app/services/ipfs/web3-storage.ts
@@ -1,14 +1,18 @@
+import { HelperService } from 'src/app/services/util/helper';
 import { Security } from './../../models/security.class';
 import { Filelike, getFilesFromPath, Web3Storage } from 'web3.storage'
-import { Base64 } from 'js-base64';
-import * as stream from 'stream';
+import * as Name from 'web3.storage/name'
+import { Revision } from 'web3.storage/name';
+import { Injectable } from '@angular/core';
+import { IEnctyptedDBObject } from 'src/app/interfaces/interfaces';
 
+@Injectable()
 export class Web3Store {
   constructor() {
   }
 
   private webstorage() {
-    return process.env.WEB3STORAGE_TOKEN
+    return ""
   }
 
   private makeStorageClient() {
@@ -20,6 +24,7 @@ export class Web3Store {
     const status = await client.status(cid);
     return status;
   }
+
   async retrieve(cid) {
     const client = this.makeStorageClient()
     const res = await client.get(cid)
@@ -43,12 +48,6 @@ export class Web3Store {
     return files;
   }
 
-  /** File objects from a json opject */
-  makeFileObject(obj, fileName) {
-    const buffer = Buffer.from(JSON.stringify(obj));
-    return new File([buffer], fileName)
-  }
-
   async getFiles(path) {
     const files = await getFilesFromPath(path)
     console.log(`read ${files.length} file(s) from ${path}`)
@@ -65,54 +64,39 @@ export class Web3Store {
     return cid;
   }
 
-  // db interfaces
-  async getDb(db_cid: string, private_key: string) {
-
-    const web3 = new Web3Store();
-    const files = await web3.retrieveFiles(db_cid);
-    const file = files[0];
-
-    if (!file) throw new Error("Db file was not found");
-
-    // the following conversion supports arabic characters, emojis and Chinese and asian character
-    // File ==> ArrayBuffer ==> Base64 ==> String ==> Object
-
-    // Conver file to ArrayBuffer
-    let buffer = await file.arrayBuffer() as any;
-
-    // Convert ArrayBuffer to Base64
-    var blob_file = new Blob([buffer], { type: 'text/plain' });
-    var serverEncryptedDb = await blob_file.text();
-
-    // Decrypt the database to user decryption level
-    const base64_str = Security.decryptString(serverEncryptedDb, private_key)
-
-    // Conver Base64 to String
-    let enctyptedStringfiedDBObject = Base64.decode(base64_str)
-
-    // Converty String to Object
-    let enctyptedDBObject = JSON.parse(enctyptedStringfiedDBObject);
-
-    return enctyptedDBObject;
-
-
+  private async _createName(value) {
+    const client = this.makeStorageClient()
+    const name = await Name.create()
+    const revision = await Name.v0(name, value)
+    await Name.publish(client, revision, name.key)
+    return { filename: name.toString(), name, revision };
   }
 
-  async storeDb(encrypteddb, private_key) {
 
-    const serverEncryptedDb = Security.encryptString(encrypteddb, private_key)
-    // convert encrypteddb string to file object
-    const buffer = Buffer.from(serverEncryptedDb);
-    const theStream = () => stream.Readable.from(buffer);
+  private async _updateName(nextValue, revision, name) {
+    const client = this.makeStorageClient()
+    const nextRevision = await Name.increment(revision, nextValue)
+    return await Name.publish(client, nextRevision, name.key)
+  }
 
-    let node_file: any = {
-      name: "encrypteddb",
-      stream: theStream
-    }
-    const web3 = new Web3Store();
+  async getRecord(recordName:string) {
+    const client = this.makeStorageClient()
+    const name = Name.parse(recordName)
+    const revision = await Name.resolve(client, name)
+    return { name, revision };
+  }
 
-    // upload to IPFS
-    const cidString = await web3.storeFiles(node_file);
-    return cidString
+  async createRecord(newValue:string) {
+    let createRecord = await this._createName(newValue)
+    console.log("==== createName()");
+    let bytes = createRecord.name.key.bytes
+    let PrivateKey = HelperService.ToBase64(bytes)
+    return {createRecord, PrivateKey}
+  }
+
+  async updateRecord(privatekeybase64:string,revision: Revision, newValue:string) {
+    let u8 = HelperService.FromBase64(privatekeybase64) as any
+    const name = await Name.from(u8)
+    return await this._updateName(newValue, revision, name)
   }
 }
