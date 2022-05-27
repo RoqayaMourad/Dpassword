59a1a4a145541ab68257faf747fd5cdc30deebd0
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-05-27 23:36:20 +0200

    IPNS object interface

--- src/app/interfaces/IPFS.interface.ts
+++ src/app/interfaces/IPFS.interface.ts
@@ -1,1 +1,8 @@
 export type IIPFSState = "Uploading" | "Downloading" | "Unsaved Changes" | "Create Item to Sync" | "Synced"
+
+export interface IIPNSObj{
+  /** used to get the CID from IPNS Records */
+  filename:string
+  /** user to update the CID in IPNS Records */
+  privatekey:string
+}
