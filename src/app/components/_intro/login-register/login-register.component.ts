6a61cffbbdf84614af1eba7ccd4056d62cd78ebc
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-05-28 01:14:17 +0200

    Register using Password and Ownership Token

--- src/app/components/_intro/login-register/login-register.component.ts
+++ src/app/components/_intro/login-register/login-register.component.ts
@@ -1,8 +1,12 @@
+import { IIPNSObj } from './../../../interfaces/IPFS.interface';
+import { HelperService } from 'src/app/services/util/helper';
 import { ModalController } from '@ionic/angular';
 import { DataService } from 'src/app/services/data/data.service';
 import { LoginService } from './../../../services/auth/login.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Web3Store } from 'src/app/services/ipfs/web3-storage';
+import { GeneratedOwnershipTokenComponent } from '../../_shared/generated-ownership-token/generated-ownership-token.component';
 
 @Component({
   selector: 'app-login-register',
@@ -14,7 +18,7 @@ export class LoginRegisterComponent implements OnInit {
   login_form: FormGroup
   register_form: FormGroup
   shown_form: "login" | "register" = "login";
-  constructor(fb: FormBuilder, private loginSrv: LoginService, private data: DataService, private modalController: ModalController) {
+  constructor(fb: FormBuilder, private loginSrv: LoginService, private data: DataService, private modalController: ModalController, private web3: Web3Store) {
     this.register_form = fb.group({
       password: ['', Validators.required],
       password_confirm: ['', Validators.required],
@@ -40,15 +44,20 @@ export class LoginRegisterComponent implements OnInit {
       this.data.alert("Invalid login fields")
       return;
     }
-    await this.loginSrv.login(this.login_form.value.email, this.login_form.value.password).then(
-      () => {
-        this.dissmiss();
-      }).catch((error) => {
-      })
+    let ownershipToken = this.login_form.get("privatekey").value;
+    let password = this.login_form.get("password").value;
+    await this.data.show_loading();
 
+    // get IPNS records
+    let IPNSObj = HelperService.decodeFileNameAndPrivateKey(ownershipToken)
+    this.web3.getRecord(IPNSObj.filename)
+    // get DB records
+    this.data.getDbFromIPFS()
+    await this.data.dismiss_loading();
   }
 
-  async register() {
+
+  async generateToken() {
     if (this.register_form.invalid) {
       this.data.alert("Invalid fields")
       return;
@@ -57,13 +66,43 @@ export class LoginRegisterComponent implements OnInit {
       this.data.alert("Confirm pasword field doesn't match password")
       return;
     }
-    await this.loginSrv.register(this.register_form.value.email, this.register_form.value.password).then(
-      () => {
-        this.dissmiss();
-      }).catch((error) => {
-        this.data.toastError(error);
-      })
+    await this.data.show_loading();
+    await this.data.initDb();
+    await this.data.uploadDbToIPFS(false)
+    let record = await this.web3.createRecord(this.data.CID);
+    let IPNSObj: IIPNSObj = {
+      filename: record.createRecord.filename,
+      privatekey: record.PrivateKey
+    }
+    this.data.CURRENT_REVISION = record.createRecord.revision;
+    this.data.IPNSObj = IPNSObj;
 
+    let ownershipToken = HelperService.encodeFileNameAndPrivateKey(IPNSObj)
+    console.log("ownershipToken", ownershipToken);
+    await this.dissmiss();
+
+    await this.data.resetStorage();
+    this.data.setMasterPassword(this.register_form.value.password);
+    await this.data.initDb();
+    await this.data.dismiss_loading();
+    this.data.IPFSState = "Create Item to Sync"
+    this.data.filter$.next("")
+
+    this.modalController.create({
+      component: GeneratedOwnershipTokenComponent,
+      cssClass: "loginRegisterModal",
+      componentProps: {
+        ownershipToken
+      },
+      backdropDismiss: true,
+    }).then((m) => {
+      m.present()
+      m.onDidDismiss().then(() => {
+        setTimeout(() => {
+          this.data.toast("Welcom To Dassword 🔐, The Password Manager build on decentralized technology", "Registred", 5000);
+        }, 1000);
+      })
+    })
   }
 
   async dissmiss() {
